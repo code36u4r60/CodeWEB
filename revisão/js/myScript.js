0027a865b3da4bc3d96da2e83d92f100b43c6ef3
code36u4r60 <code36u4r60@hotmail.com>
2017-05-23 00:44:51 +0100

    Estudo js
    Intro js

---
+++ revisão/js/myScript.js
@@ -0,0 +1,91 @@
+//Alert the user with a popup
+//alert("Seja bem vindo");
+
+//Adding in a button a function
+/*
+function myCoolFunction(){
+    alert("Ola");
+}
+*/
+
+//Scaling Functions
+/*
+function myFunctionOne() {
+    alert("One");
+}
+function myFunctionTwo() {
+    alert("Two");
+}
+function myFunctionThree() {
+    alert("Three");
+}
+*/
+
+//Changing the text in javascript 
+/*
+function changeTheText() {
+    //change the paragraph text
+document.getElementById('paragraphText').innerHTML = 'We changing the text';
+}
+*/
+
+/*
+//Variables 
+var variableNumber = 33;
+var variableName = "Eduardo";
+
+function variableFunction() {
+    alert(variableName + " " + variableNumber);
+    document.getElementById('paragraphText').innerHTML = variableName + " " + variableNumber;
+}
+*/
+
+/*
+//Adding variables 
+function addFunction() {
+    document.getElementById('paragraphText').innerHTML = variableNumber++;
+}
+*/
+
+/*
+//Adding user Input
+function addText() {
+    ;
+    var txt = document.getElementById('text').value;
+
+    document.getElementById('resultText').innerHTML += " " + txt;
+}
+*/
+
+/*
+//Calling functions with functions 
+function callWork(){
+    work("Ola Mundo");
+}
+
+function work(a) {
+    var txt = document.getElementById('textWork').value;
+
+    document.getElementById('resultWork').innerHTML += " " + txt + " " + a ;
+}
+*/
+/*
+var coolNumber = -9;
+
+function funcIf() {
+    if (coolNumber < 1) {
+        document.getElementById('resultIf').innerHTML = "< 1";
+    } else if (coolNumber == 1) {
+        document.getElementById('resultIf').innerHTML = 1;
+    } else if (coolNumber == 2) {
+        document.getElementById('resultIf').innerHTML = 2;
+    } else {
+        document.getElementById('resultIf').innerHTML = "> 2";
+    }
+}
+*/
+
+// Changing the style tags 
+function changeStyle(){
+   document.getElementById('textStyle').style.color='green'; 
+}
